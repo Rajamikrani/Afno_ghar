cab0bf061e237a7e80cf112b0e6ce738ae234c25
Raja Mikrani <rajamikrani12@gmail.com>
2026-03-30 22:19:32 +0545

    changes in cors options

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 // Handle preflight requests explicitly
-app.options("*", cors());
+app.options(/.*/, cors());
 app.use((req, res, next) => {
   console.log("📥 REQUEST:", req.method, req.url);
   next();
