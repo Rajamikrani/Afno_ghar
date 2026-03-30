48b123d1c52f52e441eb34973f8dd0432fe76d2b
Raja Mikrani <rajamikrani12@gmail.com>
2026-03-30 22:46:52 +0545

    changes in baseurl

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:8000/api/v1",
-  baseURL: "https://afno-ghar-backend.onrender.com/api/v1" ,
+baseURL: "https://afno-ghar.onrender.com/api/v1",
   withCredentials: true,
 });
 
